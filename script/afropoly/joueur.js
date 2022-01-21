3b218b6bad46c1c1a68f6019e990bd636c0c0352
Rubens Mensah <85185634+malickruru@users.noreply.github.com>
2022-01-21 22:10:01 +0000

    Update joueur.js

--- script/afropoly/joueur.js
+++ script/afropoly/joueur.js
@@ -10,16 +10,16 @@ function Joueur (photo,nom,libre,couleur,argent,position,){
 }
 
 //les instances
-chapeau=new Joueur ("../assets/cowboy-hat.png","le chapeau",true);
-cheval=new Joueur ("../assets/horse.png","le cheval",true);
-chien=new Joueur ("../assets/dog.png","le chien",true);
-brouette=new Joueur ("../assets/wheelbarrow.png","la brouette",true);
-voiture=new Joueur ("../assets/vehicle.png","la voiture",true);
-chaussure=new Joueur ("../assets/sneakers.png","la chaussure",true);
-pipe=new Joueur ("../assets/smoking-pipe.png","la pipe",true);
-fer=new Joueur ("../assets/iron.png","le fer à repasser",true);
-DeAcoudre=new Joueur ("../assets/thimble.png","le dé à coudre",true);
-bateau=new Joueur ("../assets/boat.png","le bateau",true);
+chapeau=new Joueur ("../Afropoly/assets/cowboy-hat.png","le chapeau",true);
+cheval=new Joueur ("../Afropoly/assets/horse.png","le cheval",true);
+chien=new Joueur ("../Afropoly/assets/dog.png","le chien",true);
+brouette=new Joueur ("../Afropoly/assets/wheelbarrow.png","la brouette",true);
+voiture=new Joueur ("../Afropoly/assets/vehicle.png","la voiture",true);
+chaussure=new Joueur ("../Afropoly/assets/sneakers.png","la chaussure",true);
+pipe=new Joueur ("../Afropoly/assets/smoking-pipe.png","la pipe",true);
+fer=new Joueur ("../Afropoly/assets/iron.png","le fer à repasser",true);
+DeAcoudre=new Joueur ("../Afropoly/assets/thimble.png","le dé à coudre",true);
+bateau=new Joueur ("../Afropoly/assets/boat.png","le bateau",true);
 
 const TABLEAU_JOUEUR = [
     chapeau,
